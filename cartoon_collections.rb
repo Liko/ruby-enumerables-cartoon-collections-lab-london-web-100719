b5c9647cf350dac8bb05bc78f160fe3bb26171fa
Li Xie <li_xie1992@hotmail.com>
2019-09-10 00:40:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ end
 
 def find_the_cheese(food_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food_array.include?(cheese_types) do |snack|
-    return snack
+  food_array.map do |snack|
+    return snackinclude?(cheese_types)
   end
   return nil
 end
